05b68b984fc876338a1a32becb21dfbbaf520e5d
Hamza Samad <160117284+Hamzasamad55@users.noreply.github.com>
2024-05-30 15:26:23 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+#! usr/bin/env node
+// any ko usually ham nh kerte use isski waja ye he ki ye code per asar dalta he errors ki jab k typescript hamen ye nh batata ki yahan per any ki waja se aaage errors aengen 
+// for examples
+let boy;
+//  yahan per boy k variable me any type use kiya gaya he jo hamen 
+// ye batata he ki future me yahan kuch bh a sakta he 
+console.log(boy);
+let girls;
+console.log(girls);
+export {};
